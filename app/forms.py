689e50ad5fa92f7f2e3b0a9fa0e7faa22f2c49a7
Jason <jatsburgern@gmail.com>
2022-11-01 17:18:20 -0400

    finito

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 
 class AppointmentForm(FlaskForm):
+    def validate_end_date(form, field):
+     start= datetime.combine(form.start_date.data, form.start_time.data)
+     end =datetime.combine(form.end_date.data, form.end_time.data)
+     if (start>end):
+        raise ValidationError("End date/time must come after start date/time ")
+     if(start.date!= end.date):
+        raise ValidationError("appt must start/end on same day")
+
+
     name = StringField("Name", validators=[DataRequired()])
     start_date = DateField("Start_date", validators=[DataRequired()])
     start_time = TimeField("Start_time", validators=[DataRequired()])
     end_date = DateField("End_date", validators=[DataRequired()])
-    end_time = TimeField("End_time", validators=[DataRequired()])
+    end_time = TimeField("End_time", validators=[DataRequired(), validate_end_date])
     description = TextAreaField("Description", validators=[DataRequired()])
     private = BooleanField("Private")
     submit = SubmitField("Create an appointment")
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 import os
 import sqlite3
 # import forms
 from .forms import AppointmentForm
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -12,8 +12,15 @@ DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route('/', methods=["GET", "POST"])
 def main():
-    form = AppointmentForm()
+    d = datetime.now()
+    return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
 
+@bp.route("/<int:year>/<int:month>/<int:day>")
+# @bp.route('/<year>/<month>/<day>')
+def daily(year= 1, month= 1, day=1):
+    current= datetime(year=year, month=month, day=day)
+    next= current+ timedelta(days=1)
+    form = AppointmentForm()
     if form.validate_on_submit():
         with sqlite3.connect(DB_FILE) as conn:
             curs = conn.cursor()
@@ -32,11 +39,10 @@ def main():
 
     with sqlite3.connect(DB_FILE) as conn:
         curs = conn.cursor()
-        fetched = curs.execute(
-            "SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
+        fetched = curs.execute( """SELECT id, name, start_datetime, end_datetime FROM appointments  WHERE start_datetime BETWEEN :day AND :next_day ORDER BY start_datetime;""",
+            {'day':current, 'next_day':next})
 
-    return render_template('main.html', rows=fetched, form=form)
-
+    return render_template('main.html', rows=fetched, form=form, )
 # @bp.route("/hello")
 # def hello():
 #     return render_template('items.html')
